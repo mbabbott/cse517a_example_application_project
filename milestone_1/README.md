11272579fa7608f77a952632fab72af8150e13e7
mbabbott <mbabbott@wustl.edu>
2018-03-01 20:42:57 -0600

    Added datasets and updated files for use on mbabbott's system

--- milestone_1/README.md
+++ milestone_1/README.md
@@ -20,7 +20,15 @@ README
 The README file should include
 
 * Description of what your team did
+	We used python's scipy machine learning library to implement logistic linear classification with tenfold cross validation.
 * Methods used to accomplish each part
+	As a small group, our only task was milestone 1. We constructed a dataset using downloaded midi files and python. We used the built in scipy methods to perform the machine learning task.
 * Potential difficulties faced
+	Our biggest issue was finding a dataset of midi files that had the necessary information about the musical key of each piece. Because we could not find such a dataset, we manually appended the key information of a variety of pieces taken from a repository of midi transcriptions of J.S. Bach pieces.
 * Resources used
+	SciPy
+	http://jsbach.net/midi/
+	https://en.wikipedia.org/wiki/List_of_compositions_by_Johann_Sebastian_Bach
+	
 * Description of how to run the code in the folder
+	Simply run the linModel10CV python file. In the code, it should be pointed at two .csv files: one should have bag-of-notes data from G Major pieces, the other from D Major pieces.
---
+++ milestone_1/midi2bagofratio.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 28 22:10:24 2018
+
+@author: Carter
+"""
+
+import mido
+import numpy as np
+import os
+import re
+
+def analyzeMidi(midiPath):
+    mid = mido.MidiFile(midiPath)
+    
+    bagOfNotes = np.zeros(12)
+    totalNoteCount = 0
+    
+    for i, track in enumerate(mid.tracks):
+        #print('Track {}: {}'.format(i, track.name))
+        for msg in track:
+            message = str(msg)
+            if (message[:7] == "note_on"):
+                #print(message)
+                
+                noteValIndex = message.find("note=")+5
+                if (noteValIndex >= 5):     
+                    totalNoteCount = totalNoteCount+1
+                    rawnoteval = int(message[noteValIndex:noteValIndex+2])
+                    modnoteval = rawnoteval%12
+                    
+                    bagOfNotes[modnoteval] = bagOfNotes[modnoteval] +1
+                    totalNoteCount = totalNoteCount+1
+                
+                else:
+                    print("ERROR: note not properly read")
+    ratioBag = bagOfNotes/totalNoteCount
+    return ratioBag
+
+
+#shape = (13, 34) #Dmidi
+
+key = 0; #change this to use the other dataset
+
+#directory = 'C:\\Users\\Carter\\Documents\\School\\Machine Learning\\Project\\AllMIDI\\'  
+if key == 1:
+    directory = 'C:\\Users\\Carter\\Documents\\School\\Machine Learning\\Project\\Gmidi\\'
+else:
+    directory = 'C:\\Users\\Carter\\Documents\\School\\Machine Learning\\Project\\Dmidi\\'  
+
+midiCount = 0
+for filename in os.listdir(directory):
+    if filename.endswith(".mid") or filename.endswith(".MID"):
+        midiCount = midiCount+1
+
+shape = (13, midiCount)
+dataset = np.zeros(shape)
+midiCount = 0
+
+for filename in os.listdir(directory):
+    if filename.endswith(".mid") or filename.endswith(".MID"):
+        #print(os.path.join(directory, filename))
+        path = directory + filename
+        print("Analyzing:", filename, "file#", midiCount)
+        
+        #filenameInt = re.sub('[^0-9]','', filename)
+        
+        dataset[:12, midiCount] = analyzeMidi(directory+filename)
+        #dataset[12, midiCount] = filenameInt #for any key
+        
+        if key == 1:
+            dataset[12, midiCount] = 1 #for G major
+        else:
+            dataset[12, midiCount] = 0 #for D major
+            
+            
+        midiCount = midiCount+1     
+        
+        continue
+    else:
+        print(filename, "is not a midi file!")
+        continue
+    
+#np.savetxt('bachDataset.csv', dataset, delimiter=",")
+if key == 1:
+    np.savetxt('gKeyData.csv', dataset, delimiter=",")
+else:
+    np.savetxt('dKeyData.csv', dataset, delimiter=",")
