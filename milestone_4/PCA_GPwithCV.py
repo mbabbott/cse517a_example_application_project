641438e757af5991e7786a0e6a208edd5a0f22c5
Carter Carlos <pancakeslayer@hotmail.com>
2018-05-02 18:14:34 -0500

    Add files via upload

---
+++ milestone_4/PCA_GPwithCV.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Carter
+"""
+
+import numpy as np
+
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF, Matern
+from sklearn.model_selection import KFold
+from sklearn.decomposition import PCA
+
+#trains a GP classifier and returns the predictions for Xgp_test data
+def runGP(kernel, Xgp_train, ygp_train, Xgp_test):
+    gp = GaussianProcessClassifier(kernel=kernel).fit(Xgp_train,ygp_train)
+    ygp_preds = gp.predict(Xgp_test)
+    return ygp_preds 
+
+for i in range(10):
+    print(i)
+    #import data & combine
+    gKeyData = np.genfromtxt('gKeyData.csv', delimiter=',')
+    dKeyData = np.genfromtxt('dKeyData.csv', delimiter=',')
+    fullDataset = np.concatenate((dKeyData, gKeyData), axis=1)
+    fullDataset = np.transpose(fullDataset)
+    
+    #randomize the dataset order
+    np.random.shuffle(fullDataset)
+    fullDataset = np.transpose(fullDataset)
+    
+    X_full = fullDataset[:12,:]
+    y_full = fullDataset[12, :]
+    
+    def getPCAreduce(X_trainPCA, X_testPCA, svd_bool):
+        if (svd_bool == 0):
+            pca = PCA(n_components = 'mle', svd_solver='full')
+            pca.fit(X_trainPCA.T)
+        else:
+            pca = PCA(n_components = 4)
+            pca.fit(X_trainPCA.T)
+            
+        newTrain =  pca.transform(X_trainPCA.T)
+        newTest = pca.transform(X_testPCA.T)
+        return newTrain, newTest
+        
+        
+        
+        
+    #runs 'numFolds'-fold cross validation on GP classification with kernel k_CV
+    def runCVonGP(numFolds, X, y, k_CV, PCA_bool): 
+        avErrVec = np.zeros(numFolds);
+        #numSamples = len(X[0])
+        
+        kf = KFold(n_splits=numFolds)
+        
+        X= X.T
+        y= y.T
+        
+        foldNum=0
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            
+            X_train = X_train.T
+            X_test = X_test.T
+            
+            if(PCA_bool == 1):
+                [X_train, X_test] = getPCAreduce(X_train, X_test, 0)
+                #print(X_train.shape[1], "dimensions")
+            elif(PCA_bool == 2):
+                [X_train, X_test] = getPCAreduce(X_train, X_test, 1)
+                #print(X_train.shape[1], "dimensions")
+            else:
+                X_train = X_train.T
+                X_test = X_test.T
+                #print(X_train.shape[1], "dimensions")
+    
+            
+            #train GP classifier and get predictions of test set
+            y_preds = runGP(k_CV, X_train, y_train, X_test)
+            
+            #calculate %error
+            numWrong = 0;
+            for h in range(len(y_preds)):
+                if(y_preds[h] != y_test[h]):
+                    numWrong = numWrong +1   
+                #print("prediction: ", y_preds[h], " label: ", y_test[h])
+                
+            percError = (numWrong/len(y_preds))*100
+            
+            
+            avErrVec[foldNum] = percError
+            foldNum = foldNum +1
+            
+        averageError = sum(avErrVec)/numFolds
+        return averageError
+    
+    
+    #totalErrNoPCA = runCVonGP(10, X_full, y_full, 1.0 * RBF([1.0]), 0)
+    #print("without PCA")
+    #print("Average Percent Error:", round(totalErrNoPCA, 4), "%\n")
+    #
+    #totalErrWithPCAandSVD = runCVonGP(10, X_full, y_full, 1.0 * RBF([1.0]), 1)
+    #print("with auto PCA/SVD")
+    #print("Average Percent Error:", round(totalErrWithPCAandSVD, 4), "%\n")
+    
+    totalErrWithPCA = runCVonGP(10, X_full, y_full, 1.0 * RBF([1.0]), 2)
+    print("with PCA/SVD forced to 4")
+    print("Average Percent Error:", round(totalErrWithPCA, 4), "%")
+    
+    
+    
+        
+        
---
+++ milestone_4/linModel10CV_2.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 01:56:14 2018
+
+@author: Carter
+"""
+
+import numpy as np
+from sklearn import linear_model
+
+
+#fullDataset = np.genfromtxt('bachDatasetWithLabels.csv', delimiter=',')
+gKeyData = np.genfromtxt('gKeyData.csv', delimiter=',')
+dKeyData = np.genfromtxt('dKeyData.csv', delimiter=',')
+
+fullDataset = np.concatenate((dKeyData, gKeyData), axis=1)
+
+fullDataset = np.transpose(fullDataset)
+np.random.shuffle(fullDataset)
+
+fullDataset = np.transpose(fullDataset)
+
+
+
+X = fullDataset[:12,:]
+y = fullDataset[12, :]
+
+
+#set the training data to literally be the label
+#for i in range(0,12):
+ #   X[i,:] = y
+
+
+numSamples = len(X[0])
+splitCol = int(numSamples*.66)
+
+#for k in range(len(y)):
+#    if (y[k] == 14):
+#        y[k] = 1
+#        
+#    else:
+#        y[k] = 0
+
+X_train = X[:, :splitCol]
+#X_train = X_train.reshape((splitCol,12))
+X_train = X_train.T
+y_train = y[:splitCol]
+#y_train = y_train.reshape((splitCol, 1))
+
+X_test = X[:, splitCol:]
+#X_test = X_test.reshape((numSamples-splitCol,12))
+X_test = X_test.T
+y_test = y[splitCol:]
+#y_test = y_test.reshape((numSamples-splitCol,1))
+
+
+
+logModel = linear_model.LogisticRegressionCV(cv=10)
+#logModel = linear_model.LinearRegression()
+logModel.fit(X_train, y_train)
+
+y_preds = logModel.predict(X_test)
+
+#for h in range(len(y_preds)):
+#    print("prediction: ", y_preds[h], " label: ", y_test[h])
+    
+
+#print("Mean squared error: %.2f" % math.sqrt(mean_squared_error(y_test, y_preds)))
+
+
+numWrong = 0;
+for h in range(len(y_preds)):
+    if(y_preds[h] != y_test[h]):
+        numWrong = numWrong +1   
+    #print("prediction: ", y_preds[h], " label: ", y_test[h])
+    
+percError = (numWrong/len(y_preds))*100
+print(percError)
+
+
+
+
+
